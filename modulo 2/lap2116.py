315ad2943ab6d7dff66a54793e293d55316598ae
ED821 <foserone532@gmail.com>
2022-09-20 08:41:29 -0500

    laboratorio 2 1 1 6

---
+++ modulo 2/lap2116.py
@@ -0,0 +1,9 @@
+'''
+Descripcion:Pograma muy facil de la funcion de Python
+Autor:Edgar Hernandez
+Fecha:20/septiembre/2022cd
+
+
+print ("Hola Mundo")
+print ("Soy paquito")
+print("Bienvenido a Python")
